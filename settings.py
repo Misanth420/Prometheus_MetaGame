9b529365049b7b9a5c5a18e62fc6f9f92a3794d6
Misanth420 <petricmat@gmail.com>
2023-01-28 10:49:55 +0100

    formatting and base db, version control
    done some formatting, separated the testing cogs/cmds, added a base db;
    need to update sample env to reflectchanges as well, commiting for version control

--- settings.py
+++ settings.py
@@ -5,8 +5,6 @@ from logging.config import dictConfig
 from dotenv import load_dotenv
 
 
-
-
 load_dotenv()
 
 DISCORD_API_SECRET = os.getenv("DISCORD_API_TOKEN")
@@ -15,6 +13,10 @@ D_REPORT_CHAN_ID = int(os.getenv("REPORT_CHANNEL_ID"))
 D_REP_MENU_BUILD_ROLE = os.getenv("DISCORD_CALL_REPORTMENU_REQUIRED_ROLE")
 D_TIMEOUT_USE_ROLE = os.getenv("DISCORD_TIMEOUT_POLL_REQUIRED_ROLE")
 
+DB_DATABASE = os.getenv("DB_DATABASE")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_URL = os.getenv("DB_URI")
 
 
 BASE_DIR = pathlib.Path(__file__).parent
@@ -22,50 +24,45 @@ BASE_DIR = pathlib.Path(__file__).parent
 
 CMDS_DIR = BASE_DIR / "cmds"
 COGS_DIR = BASE_DIR / "cogs"
+TESTCOGS_DIR = BASE_DIR / "testcogs"
 
 
 LOGGING_CONFIG = {
-    "version": 1, 
-    "disabled_existing_loggers": False, 
-    "formatters":{
-        "verbose":{
+    "version": 1,
+    "disabled_existing_loggers": False,
+    "formatters": {
+        "verbose": {
             "format": "%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s"
         },
-        "standard":{
-            "format": "%(levelname)-10s - %(name)-15s : %(message)s"
-        }
-    }, 
-    "handlers":{
+        "standard": {"format": "%(levelname)-10s - %(name)-15s : %(message)s"},
+    },
+    "handlers": {
         "console": {
-            'level': "DEBUG", 
-            'class': "logging.StreamHandler",
-            'formatter': "standard"
-        }, 
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
         "console2": {
-            'level': "WARNING", 
-            'class': "logging.StreamHandler",
-            'formatter': "standard"
-        }, 
+            "level": "WARNING",
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
         "file": {
-            'level': "INFO", 
-            'class': "logging.FileHandler",
-            'filename': "logs/infos.log",
-            'mode': "w", 
-            'formatter': "verbose"
-        }, 
-    }, 
-    "loggers":{
-        "bot": {
-            'handlers': ['console'],
-            "level": "INFO", 
-            "propagate": False
-        }, 
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "logs/infos.log",
+            "mode": "w",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "bot": {"handlers": ["console"], "level": "INFO", "propagate": False},
         "discord": {
-            'handlers': ['console2', "file"],
-            "level": "INFO", 
-            "propagate": False
-        }
-    }
+            "handlers": ["console2", "file"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
 }
 
 dictConfig(LOGGING_CONFIG)
